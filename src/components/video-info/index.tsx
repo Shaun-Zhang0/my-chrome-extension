a5c4ccd7542714f88aaf8fa53d6a33be4c0288fc
Shaun-Zhang0 <724892908@qq.com>
2021-07-08 00:39:17 +0800

    feat: 开始制作视频页的样式部分

--- src/components/video-info/index.tsx
+++ src/components/video-info/index.tsx
@@ -1,12 +1,19 @@
 import React, {useEffect, useState} from "react"
 
+require("./index.less");
+
 const VideoInfo = () => {
+    const [videoUrl, setVideoUrl] = useState("");
+    const getVideoUrl = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setVideoUrl(e.target.value)
+    };
     return <div className={"video-form"}>
         <div className={"form-title"}>计算视频地址的码率</div>
         <div className={"form-item"}>
-            <label>视频地址</label>
-            <input className={'form-input'}/>
+            <label className={'form-label'}>视频地址</label>
+            <input className={'form-input'} onChange={getVideoUrl} value={videoUrl}/>
         </div>
+        <div className={"form-submit"}>确定</div>
     </div>
 };
 export default React.memo(VideoInfo);
